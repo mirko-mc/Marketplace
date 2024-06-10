cab834ca95caa4c62d68dcf729c6e1fc128f33db
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-06-11 00:14:09 +0200

    WIP - delete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,36 @@
 const FETCHURL = "https://striveschool-api.herokuapp.com/api/product/";
 const FETCHTOKEN =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjY3NDRhNjdmNmI0YjAwMTU0MjhmYzciLCJpYXQiOjE3MTgwNDM4MTQsImV4cCI6MTcxOTI1MzQxNH0.SXzbkbBeQ3k_Id3jleh3mqKxiDVVMO0B3PmJiY3Q7Sk";
+const ID = "666757ac7f6b4b0015428fdd";
 
 document.addEventListener("DOMContentLoaded", () => {
   postData();
   getData();
+  deleteData();
+  putData();
 });
 
 async function getData() {
   await fetch(FETCHURL, {
     method: "GET",
     headers: { Authorization: FETCHTOKEN, "Content-Type": "application/json" },
-  }).then((response) => {
-    console.log("GETDATA => response\n", response);
-    return response.json();
-  }).then((data) => {
-    console.log("GETDATA => data\n", data);
-  });
+  })
+    .then((response) => {
+      // console.log("GETDATA => response\n", response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log("GETDATA => data\n", data);
+      return data;
+    });
 }
-
-
 
 async function postData() {
   await fetch(FETCHURL, {
     method: "POST",
     headers: {
       "content-type": "application/json",
-      "Authorization": FETCHTOKEN,
+      Authorization: FETCHTOKEN,
     },
     body: JSON.stringify({
       name: "name2",
@@ -38,11 +42,22 @@ async function postData() {
   })
     .then((response) => {
       console.log("POSTDATA => response\n", response);
-    //   return response.json();
     })
-    // .then((data) => {
-    //   console.log("POSTDATA => data\n", data);
-    // })
+    .catch((error) => {
+      console.log("POSTDATA => error\n", error);
+    });
+}
+async function deleteData() {
+  await fetch(FETCHURL + ID, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      Authorization: FETCHTOKEN,
+    },
+  })
+    .then((response) => {
+      console.log("POSTDATA => response\n", response);
+    })
     .catch((error) => {
       console.log("POSTDATA => error\n", error);
     });
